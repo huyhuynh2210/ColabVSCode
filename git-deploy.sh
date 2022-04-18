da714638e45399b5b7e31ecf62951e719b358c46
huyhuynh2210 <19521609@gm.uit.edu.vn>
2022-04-18 07:23:40 +0000

    btlop.n06

---
+++ git-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+read -r -p 'Commit message: ' desc  # prompt user for commit message
+git add .                           # track all files
+git add -u                          # track deletes
+git status                          
+git commit -m "$desc"               # commit with message
+git push
---
+++ git-signin.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git config --global user.name huyhuynh2210
+git config --global user.email 19521609@gm.uit.edu.vn
